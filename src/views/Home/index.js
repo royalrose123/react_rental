9cab27341016ef41916b472cde97b143a12244d9
Royal Chen <royal.chen@cloud-interactive.com>
2020-09-17 23:58:22 +0800

    Add apollo/react-hooks

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -2,10 +2,14 @@ import React from 'react'
 // import PropTypes from 'prop-types'
 import classnames from 'classnames/bind'
 import styles from './style.module.scss'
+import { useQuery } from '@apollo/react-hooks'
 
 // Components
 
 // Lib MISC
+
+// gql
+import { USER_LIST } from './gql'
 
 // Variables / Functions
 const cx = classnames.bind(styles)
@@ -14,8 +18,12 @@ export const propTypes = {}
 
 function Home(props) {
   // const { } = props;
+  const { data, loading: suggestionLoading } = useQuery(USER_LIST)
 
-  return <div className={cx('home')}>Hssome</div>
+  console.log('data', data)
+  console.log('suggestionLoading', suggestionLoading)
+
+  return <div className={cx('home')}>Home</div>
 }
 
 Home.propTypes = propTypes
