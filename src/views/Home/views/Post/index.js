7197d599026a58ed3e17ceb041e002eb570bd88b
Royal Chen <royal.chen@cloud-interactive.com>
2020-09-21 23:28:29 +0800

    Update Post component by poster

--- src/views/Home/views/Post/index.js
+++ src/views/Home/views/Post/index.js
@@ -3,6 +3,7 @@ import React from 'react'
 import classnames from 'classnames/bind'
 
 // Components
+import postPosterImage from 'assets/images/post_poster.jpeg'
 
 // Style
 import styles from './style.module.scss'
@@ -13,7 +14,14 @@ const cx = classnames.bind(styles)
 export const propTypes = {}
 
 function Post(props) {
-  return <div className={cx('post')}>Post</div>
+  return (
+    <div className={cx('post')}>
+      <div className={cx('post__poster')}>
+        <img className={cx('post__poster-image')} src={postPosterImage} />
+        <div className={cx('post__poster-title')}>房屋刊登</div>
+      </div>
+    </div>
+  )
 }
 
 Post.propTypes = propTypes
