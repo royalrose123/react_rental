3b4bff50ae872416ad20584725ff892b16bb349e
Royal Chen <royal.chen@cloud-interactive.com>
2020-09-25 14:58:36 +0800

    Update House Map by google map with native script

---
+++ src/views/Home/views/House/components/Map/hooks/useMapEffect.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+import { isEmpty } from 'lodash'
+
+function useMapEffect() {
+  const [position, setPosition] = useState({})
+  const [map, setMap] = useState(null)
+
+  console.log('map 11111', map)
+
+  useEffect(() => {
+    if (isEmpty(position)) {
+      window.navigator.geolocation.getCurrentPosition((position) => {
+        const coords = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        }
+
+        setPosition(coords)
+      })
+    }
+  }, [position])
+
+  useEffect(() => {
+    if (!isEmpty(position)) {
+      const newMap = new window.google.maps.Map(document.getElementById('map'), {
+        center: position,
+        zoom: 15,
+        mapTypeId: 'roadmap',
+      })
+
+      setMap(newMap)
+    }
+  }, [position])
+}
+
+export default useMapEffect
