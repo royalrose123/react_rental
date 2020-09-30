4a546df1f85a117e4b55f09d5ffa5c933076cce5
Royal Chen <royal.chen@cloud-interactive.com>
2020-09-30 23:38:46 +0800

    Add add svg file

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,4 +1,9 @@
 import { ReactComponent as NavLogo } from './svg/nav_logo.svg'
 import { ReactComponent as Heart } from './svg/heart.svg'
+import { ReactComponent as Add } from './svg/add.svg'
 
-export default { NavLogo, Heart }
+export default {
+  NavLogo,
+  Heart,
+  Add,
+}
