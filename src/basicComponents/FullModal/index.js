3682e3ad0ec89b5f802ccfd86a80e3cceadec47e
Royal Chen <royal.chen@cloud-interactive.com>
2020-10-02 20:31:38 +0800

    Add FullModal Component

---
+++ src/basicComponents/FullModal/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classnames from 'classnames/bind'
+
+// Components
+import Modal from 'basicComponents/Modal'
+
+// Style
+import styles from './style.module.scss'
+
+// Variables / Functions
+const cx = classnames.bind(styles)
+
+export const propTypes = {
+  children: PropTypes.object,
+}
+
+const defaultProps = {}
+
+function FullModal(props) {
+  const { children, ...restProps } = props
+
+  return (
+    <Modal className={cx('full-modal')} isShown hasBack {...restProps}>
+      {children}
+    </Modal>
+  )
+}
+
+FullModal.propTypes = propTypes
+FullModal.defaultProps = defaultProps
+
+export default FullModal
