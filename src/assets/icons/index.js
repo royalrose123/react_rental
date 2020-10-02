d7af5282b8187f38f3272255772923c6ebce1be3
Royal Chen <royal.chen@cloud-interactive.com>
2020-10-02 17:07:28 +0800

    Add Next and Previous svg files

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,9 +1,13 @@
 import { ReactComponent as NavLogo } from './svg/nav_logo.svg'
 import { ReactComponent as Heart } from './svg/heart.svg'
 import { ReactComponent as Add } from './svg/add.svg'
+import { ReactComponent as Previous } from './svg/previous.svg'
+import { ReactComponent as Next } from './svg/next.svg'
 
 export default {
   NavLogo,
   Heart,
   Add,
+  Previous,
+  Next,
 }
