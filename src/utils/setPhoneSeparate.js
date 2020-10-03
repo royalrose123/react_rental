6d1556f4b6dfd7fd0fb5448600e4d428584d47bd
Royal Chen <royal.chen@cloud-interactive.com>
2020-10-03 12:30:40 +0800

    Add setPhoneSeparate util

---
+++ src/utils/setPhoneSeparate.js
@@ -0,0 +1,22 @@
+export function setPhoneSeparate(phone) {
+  phone = phone.toString()
+
+  let count = 0
+  let newPhone = ''
+
+  for (let i = phone.length - 1; i >= 0; i--) {
+    count++
+    newPhone += phone[i]
+    if (i >= 4) {
+      if (count >= 3) {
+        newPhone += '-'
+        count = 0
+      }
+    }
+  }
+  newPhone = newPhone.split('').reverse().join('')
+
+  if (newPhone[0] === '-') newPhone = `0${newPhone.substr(1)}`
+
+  return newPhone
+}
