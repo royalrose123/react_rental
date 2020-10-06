7df4ce0d021b3ed4ef5c82e839ed3824ae16c5dd
Royal Chen <royal.chen@cloud-interactive.com>
2020-10-06 17:57:39 +0800

    Update Post Form validation and initialValues

--- src/views/Home/views/Post/components/Form/validation.js
+++ src/views/Home/views/Post/components/Form/validation.js
@@ -3,10 +3,10 @@ import * as Yup from 'yup'
 export const schema = Yup.object().shape({
   city: Yup.string().required('此欄位為必填'),
   distinct: Yup.string().required('此欄位為必填'),
-  floor: Yup.number().typeError('此欄位為數字').positive('此欄位不接受負數').required('此欄位為必填'),
+  floor: Yup.number().typeError('此欄位為數字').integer('此欄位需為整數').min(1, '此欄位不可為 0').required('此欄位為必填'),
   houseDetail: Yup.string().required('此欄位為必填'),
-  livingroomAmount: Yup.number().typeError('此欄位為數字').positive('此欄位不接受負數').required('此欄位為必填'),
-  price: Yup.number().typeError('此欄位為數字').positive('此欄位不接受負數').required('此欄位為必填'),
+  livingroomAmount: Yup.number().typeError('此欄位為數字').integer('此欄位需為整數').min(0, '此欄位不接受負數').required('此欄位為必填'),
+  price: Yup.number().typeError('此欄位為數字').integer('此欄位需為整數').min(1, '此欄位不可為 0').required('此欄位為必填'),
   require: Yup.object().shape({
     cook: Yup.string().required('此欄位為必填'),
     deposit: Yup.string().required('此欄位為必填'),
@@ -16,13 +16,13 @@ export const schema = Yup.object().shape({
     pet: Yup.string().required('此欄位為必填'),
   }),
 
-  restroomAmount: Yup.number().typeError('此欄位為數字').positive('此欄位不接受負數').required('此欄位為必填'),
-  roomAmount: Yup.number().typeError('此欄位為數字').positive('此欄位不接受負數').required('此欄位為必填'),
+  restroomAmount: Yup.number().typeError('此欄位為數字').integer('此欄位需為整數').min(0, '此欄位不接受負數').required('此欄位為必填'),
+  roomAmount: Yup.number().typeError('此欄位為數字').integer('此欄位需為整數').min(0, '此欄位不接受負數').required('此欄位為必填'),
   roomType: Yup.string().required('此欄位為必填'),
-  size: Yup.number().typeError('此欄位為數字').positive('此欄位不接受負數').required('此欄位為必填'),
+  size: Yup.number().typeError('此欄位為數字').integer('此欄位需為整數').min(1, '此欄位不可為 0').required('此欄位為必填'),
   street: Yup.string().required('此欄位為必填'),
   surrounding: Yup.string().required('此欄位為必填'),
   title: Yup.string().required('此欄位為必填'),
-  totalFloor: Yup.number().typeError('此欄位為數字').positive('此欄位不接受負數').required('此欄位為必填'),
+  totalFloor: Yup.number().typeError('此欄位為數字').integer('此欄位需為整數').min(1, '此欄位不可為 0').required('此欄位為必填'),
   fileList: Yup.array().required('至少選一張照片上傳'),
 })
